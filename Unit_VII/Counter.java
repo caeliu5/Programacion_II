c6622a86ff99c3c51c5879ae3af8661c8788a3be
h04dn <83139871+h04dn@users.noreply.github.com>
2021-04-27 13:53:07 -0400

    Update Counter.java

--- Unit_VII/Counter.java
+++ Unit_VII/Counter.java
@@ -1,18 +1,14 @@
 public class Counter{
 
-    private int count=1;
-    private int step=1;
-    private final int MAX_COUNT=50;
+    private int Count=1;
+    private int Step=1;
+    private final int MAX_COUNT=10;
 
     public void displayCount(){
 
-      //No se muestra en CounterTest 
-      while (MAX_COUNT <= count ) {
-
-          System.out.println(count);
-          count=count+step=1;
-       }
-            
-
+        while (MAX_COUNT >= Count){
+        System.out.println(Count);
+        Count=Count + Step;
+        }
     }
 }
